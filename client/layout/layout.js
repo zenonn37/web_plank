f3b85cf8fb7f734715213441a1a0101ff4ce121e
Christopher <chriscrowd@gmail.com>
2015-10-21 14:23:51 -0400

    fixed sidenav

--- client/layout/layout.js
+++ client/layout/layout.js
@@ -12,13 +12,29 @@ Template.layout.helpers({
   },
 });
 
-Template.layout.onRendered(function() {
-   this.$(".button-collapse").sideNav({
-     menuWidth:240,
-     edge:'left',
-     closeOnClick:true
-   });
-});
+Template.layout.rendered = function() {
+Meteor.setTimeout(function() {
+
+  this.$('.dropdown-button').dropdown({
+    inDuration: 300,
+    outDuration: 225,
+    constrain_width: false,
+    hover: false,
+    alignment: 'right',
+    gutter: 0,
+    belowOrigin: true
+  });
+
+  this.$('.button-collapse').sideNav({
+    menuWidth: 240,
+    activationWidth: 70,
+    closeOnClick: true,
+      edge: 'left', // Choose the horizontal origin
+  });
+
+}.bind(this), 200);
+};
+
 
 Template.layout.events({
   "click .open-menu": function(event, template){
