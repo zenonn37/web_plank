8c46c060a545dfa233d3432622b85822b203aa41
Christopher <chriscrowd@gmail.com>
2015-11-02 14:14:13 -0500

    night

--- models/members-model.js
+++ models/members-model.js
@@ -1,5 +1,17 @@
 Members = new Mongo.Collection("members");
-Members.allow({ 
+Members.allow({
+  insert: function(){
+    return false;
+  },
+  update: function(){
+    return false;
+  },
+  remove: function(){
+    return false;
+  }
+});
+
+Members.deny({
   insert: function(){
     return true;
   },
