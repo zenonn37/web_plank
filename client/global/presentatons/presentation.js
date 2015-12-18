8a7a45a19a457520a9ccbab195480602b32feaa3
Christopher <chriscrowd@gmail.com>
2015-12-18 14:01:04 -0500

    fixed overlay issue

--- client/global/presentatons/presentation.js
+++ client/global/presentatons/presentation.js
@@ -10,13 +10,14 @@ Presentation = {
       var $dataCoverOverlay = $('.data-cover-overlay');
 
       var globalDataSeq = [
-        {e:$dataCoverOverlay,p:{opacity:0.9},o:{duration:400,easing:"ease-in"}},
-        {e:$globalWrap,p:{left:'150px'},o:{duration:1200,easing:"spring"}},
+        {e:$dataCoverOverlay,p:{left:'0px'},o:{duration:100,easing:"ease-in"}},
+        {e:$dataCoverOverlay,p:{opacity:0.9},o:{duration:200,easing:"ease-in"}},
+        {e:$globalWrap,p:{left:'150px'},o:{duration:800,easing:"spring"}},
         {e:$panelDecor,p:{left:'0px'},o:{stagger:100,duration:200,easing:"ease-in"}},
         {e:$qContent,p:{opacity:0.9},o:{stagger:100,duration:200,easing:"ease-in"}},
         {e:$globalQuick,p:{opacity:0.9},o:{stagger:100,duration:100,easing:"ease-in"}},
-        {e:$closeDataPopout,p:{rotateZ:'180deg',opacity:1.0},o:{stagger:100,duration:500,easing:"ease-in"}},
-        {e:$dataCoverOverlay,p:{opacity:0.4},o:{stagger:100,duration:400,easing:"ease-in"}}
+        {e:$closeDataPopout,p:{rotateZ:'180deg',opacity:1.0},o:{stagger:100,duration:300,easing:"ease-in"}},
+        {e:$dataCoverOverlay,p:{opacity:0.4},o:{stagger:100,duration:200,easing:"ease-in"}}
 
       ];
         $.Velocity.RunSequence(globalDataSeq);
@@ -32,13 +33,14 @@ Presentation = {
     var $dataCoverOverlay = $('.data-cover-overlay');
 
      var globalDataSeq = [
-       {e:$dataCoverOverlay,p:{opacity:0.9},o:{duration:200,easing:"ease-out"}},
-       {e:$closeDataPopout,p:{rotateZ:'-180deg',opacity:0},o:{stagger:100,duration:500,easing:"ease-out"}},
+       {e:$dataCoverOverlay,p:{opacity:0.9},o:{duration:100,easing:"ease-out"}},
+       {e:$closeDataPopout,p:{rotateZ:'-180deg',opacity:0},o:{stagger:100,duration:100,easing:"ease-out"}},
        {e:$globalQuick,p:{opacity:0},o:{stagger:50,duration:100,easing:"ease-out"}},
        {e:$qContent,p:{opacity:0},o:{stagger:50,duration:100,easing:"ease-out"}},
        {e:$panelDecor,p:{left:'-40px'},o:{stagger:100,duration:200,easing:"ease-out"}},
-       {e:$globalWrap,p:{left:'-3000px'},o:{stagger:100,duration:1200,easing:"ease-out"}},
-         {e:$dataCoverOverlay,p:{opacity:0},o:{stagger:100,duration:200,easing:"ease-in"}}
+       {e:$globalWrap,p:{left:'-3000px'},o:{stagger:100,duration:300,easing:"ease-out"}},
+       {e:$dataCoverOverlay,p:{opacity:0},o:{stagger:100,duration:200,easing:"ease-in"}},
+       {e:$dataCoverOverlay,p:{left:'-2000px'},o:{duration:100,easing:"ease-in"}}
 
 
 
