b5165b185eae0a58cd6e8c5fd6c1d8a70e01dfef
Christopher <chriscrowd@gmail.com>
2016-01-26 20:23:16 -0500

    invite code working finish accept and login

--- server/modules/connect-invite.js
+++ server/modules/connect-invite.js
@@ -17,25 +17,35 @@ var template =   _setupEmail(docs.token);
 };
 //private function to insert connect invite toDB
 var _insertConnect = function(docs) {
-  Companion.insert(docs,function(err) {
-if (err) {
-  console.log(err);
-}
-  });
+  Companion.insert(docs);
+
+
 };
 
 //private function to pull from settings fro email setup
 var _setupEmail = function(token) {
+  console.log('called in  setup email');
   var domain = Meteor.settings.private.domain;
-  var url = 'http://${domain}/invite/${token}';
-
+  console.log(domain);
+  console.log(token);
+  var url = `http://${ domain }/invite/${ token }`;
+console.log(url);
 SSR.compileTemplate( 'connect', Assets.getText( 'email/templates/connect.html' ) );
 var html = SSR.render( 'connect', { url: url } );
-
+console.log(html);
 return html;
 };
 //private function send email
 var _sendConnectEmail = function(email, template) {
+  console.log('called in send email');
+  var settings = Meteor.settings.private.mailgun,
+    mailGun = new Mailgun({
+      apiKey:settings.apiKey,
+      domain:settings.domain
+    }),
+    address = settings.domain;
+
+    console.log(email);
   mailGun.send({
     from:'plank@plankcompany.io',
     to:email,
@@ -45,6 +55,8 @@ var _sendConnectEmail = function(email, template) {
   },function(err, body) {
     if (err) {
       console.log(err.reason);
+    }else{
+      console.log('no email sent ' + body);
     }
   });
 };
